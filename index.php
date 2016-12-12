e64f31e38717897685b77e11b76380d39fc3661c
Alexander Hesse <alexander.hesse@sandstorm-media.de>
2016-12-12 11:21:22 +0100

    [FEATURE] all quotes as json

--- index.php
+++ index.php
@@ -1,16 +1,38 @@
 <?php
 require __DIR__ . '/vendor/autoload.php';
 
-$pdo = new PDO('mysql:host=localhost;dbname=kanguru', 'root', '');
+function setup() {
+	return new PDO('mysql:host=localhost;dbname=kanguru', 'root', '');
+}
 
 // Your App
 $app = new Bullet\App();
-$app->path('/', function($request) {
-    return "Hello World!";
+$app->path('/', function ($request) {
+	return "Hello World!";
 });
 
-$app->path('/test', function($request) {
-  return "klaus";
+$app->path('/listAll', function ($request) {
+
+	$pdo = setup();
+
+	$array = [];
+	foreach($pdo->query("SELECT * from quotes") as $row) {
+		array_push($array, $row);
+	}
+
+	echo json_encode($array);
+});
+
+$app->path('/get/', function ($request) {
+
+	$pdo = setup();
+
+	$array = [];
+	foreach($pdo->query("SELECT * from quotes") as $row) {
+		array_push($array, $row);
+	}
+
+	echo json_encode($array);
 });
 
 // Run the app! (takes $method, $url or Bullet\Request object)
