42c0511583c2b673bd8bdd518e47f37a40762c53
Alexander Hesse <alexander.hesse@sandstorm-media.de>
2016-12-20 12:22:05 +0100

    [TASK] got now 61 quotes

--- main.js
+++ main.js
@@ -6,8 +6,10 @@ var endpoint = urlParts[0] + "//" + urlParts[2] + "/api.php";
 
 $(document).ready(function() {
 
+	var minKeyValue = 0;
+	var maxKeyValue = Object.keys(quotes).length -1;
 
-	console.log(quotes);
+	console.log(minKeyValue, maxKeyValue);
 
 	function getRandomQuote() {
 		var quote = apiCall("random");
--- quotes.js
+++ quotes.js
@@ -83,5 +83,230 @@ var quotes = {
         "quote": "Wenn man ein 0:2 kassiert, dann ist ein 1:1 nicht mehr möglich",
         "author": "Satz des Pythagoras",
         "original": "Aleksander Ristic"
-    }
+    },
+	"17": {
+		"quote": "Hier bin ich Mensch, hier kauf' ich ein.",
+		"author": "Friedrich Schiller",
+		"original": "Werbespruch des Drogeriemarktes dm"
+	},
+	"18": {
+		"quote": "Ein Experte ist ein Mann, der hinterher genau sagen kann, warum seine Prognose nicht gestimmt hat",
+		"author": "Peter Hartz",
+		"original": "Winston Churchill"
+	},
+	"19": {
+		"quote": "Rock'n'Roll ain't noise pollution. Rock'n'Roll is just Rock'n'Roll. Yeah.",
+		"author": "Walter von der Vogelweide",
+		"original": "Rock 'N' Roll Ain't Noise Pollution von AC/DC"
+	},
+	"20": {
+		"quote": "Das wird doch alles von den Medien hochsterilisiert.",
+		"author": "Osama bin Laden",
+		"original": "Bruno Labbadia"
+	},
+	"21": {
+		"quote": "Liebe deinen Nächsten wie dich selbst.",
+		"author": "Kurt Cobain",
+		"original": "An den verschiedensten Stellen in der Bibel"
+	},
+	"22": {
+		"quote": "Handle nur nach derjenigen Maxime, durch die du zugleich wollen kannst, dass sie ein allgemeines Gesetz werde.",
+		"author": "Silvio Berlusconi",
+		"original": "Immanuel Kant"
+	},
+	"23": {
+		"quote": "Die Proletarier haben nichts zu verlieren als ihre Ketten.",
+		"author": "Heidi Klum",
+		"original": "Manifest der Kommunistischen Partei"
+	},
+	"24": {
+		"quote": "Schön, die Phönizier haben das Geld erfunden. Aber warum so wenig?",
+		"author": "Buddha",
+		"original": "Johann Nepomuk Eduard Ambrosius Nestroy"
+	},
+	"25": {
+		"quote": "Ihr Völker der Welt, schaut auf diese Stadt!",
+		"author": "Klaus Wowereit",
+		"original": "Ernst Reuter"
+	},
+	"26": {
+		"quote": "Die Macht ist nicht etwas, was man erwirbt, wegnimmt, teilt, was man bewahrt oder verliert; die Macht ist etwas, was sich von unzähligen Punkten aus und im Spiel ungleicher und beweglicher Beziehungen vollzieht.",
+		"author": "Darth Vader",
+		"original": "Michel Foucault in Sexualität und Wahrheit"
+	},
+	"27": {
+		"quote": "Niemand hat die Absicht, eine Mauer zu errichten.",
+		"author": "Bob der Baumeister",
+		"original": "Walter Ulbricht"
+	},
+	"28": {
+		"quote": "Verdammt sei der erste, der ein Stück Land mit einem Zaun umgab, und sagte: ”Dies gehört mir.” und verdammt seien die Leute, die einfältig genug waren, ihm zu glauben.",
+		"author": "Jaques Chirac/Charles de Gaulle/Napoleon I",
+		"original": "Jean-Jaques Rousseau"
+	},
+	"29": {
+		"quote": "There's no busines like show business",
+		"author": "Muammar al-Gaddafi",
+		"original": "Song von Irving Berlin, der für das Musical 'Annie get your gun' komponiert wurde"
+	},
+	"30": {
+		"quote": "Nichts ist mächtiger als eine Idee, deren Zeit gekommen ist.",
+		"author": "Paris Hilton",
+		"original": "Victor Hugo"
+	},
+	"31": {
+		"quote": "Das Boot ist voll.",
+		"author": "Noah, Sohn des Lamech",
+		"original": "Eduard von Steiger, 30. August 1940"
+	},
+	"32": {
+		"quote": "Macht kaputt, was euch kaputt macht!",
+		"author": "Axel Springer",
+		"original": "Lied von Ton Steine Scherben"
+	},
+	"33": {
+		"quote": "Je öfter eine Dummheit wiederholt wird, desto mehr bekommt sie den Anschein der Klugheit.",
+		"author": "Lehman Brothers",
+		"original": "Voltaire"
+	},
+	"34": {
+		"quote": "Wer sagt: hier herrscht Freiheit, der lügt, denn Freiheit herrscht nicht.",
+		"author": "Barack Obama",
+		"original": "Erich Fried"
+	},
+	"35": {
+		"quote": "Man muss eine Weile nachdenken, um zu erkennen, dass man unglücklich ist, doch es lohnt sich.",
+		"author": "Sigmund Freud",
+		"original": "Shirley Bassey"
+	},
+	"36": {
+		"quote": "In diesem Fall ziehen Sie eine Maske schnell zu sich heran und platzieren diese fest auf Mund und Nase. Danach helfen Sie Kindern und hilfsbedürftigen Personen.",
+		"author": "Batman zu Robin",
+		"original": "Typische Ansage von Flugbegleitern, die die Sicherheitsmaßnahmen vor einem Flug erklären"
+	},
+	"37": {
+		"quote": "Wozu ist das? Das ist blaues Licht. Und was macht es? Es leuchtet blau.",
+		"author": "Deutsche Hochschule der Polizei",
+		"original": "Rambo 3"
+	},
+	"38": {
+		"quote": "Dream a little dream of me.",
+		"author": "Freddy Krueger",
+		"original": "Song von Kahn, Andre und Schwandt, vielfach interpretiert (The Mamas and the Papas u.a.)"
+	},
+	"39": {
+		"quote": "Ich mache nie Voraussagen und werde das auch niemals tun.",
+		"author": "Nostradamus",
+		"original": "Paul Gascoigne"
+	},
+	"40": {
+		"quote": "I want you to want me. I need you to need me.",
+		"author": "Angebot zur Nachfrage",
+		"original": "Song von Cheap Trick"
+	},
+	"41": {
+		"quote": "Wer kämpft, kann verlieren. Wer nicht kämpft, hat schon verloren.",
+		"author": "Optimus Prime",
+		"original": "Bertolt Brecht"
+	},
+	"42": {
+		"quote": "Köpfchen in das Wasser, Schwänzchen in die Höh.",
+		"author": "CIA Verhörmethode",
+		"original": "Kinderlied Alle meine Entchen"
+	},
+	"43": {
+		"quote": "Ich schaue mir meine Filme nie an. Sie sind mir zu brutal.",
+		"author": "Jennifer Aniston",
+		"original": "Charles Bronson"
+	},
+	"44": {
+		"quote": "Jedes Publikum kriegt die Vorstellung, die es verdient.",
+		"author": "Mario Barth",
+		"original": "Curt Goetz"
+	},
+	"45": {
+		"quote": "Das sind Gefühle, wo man nur schwer beschreiben kann.",
+		"author": "Thomas Mann bei der Nobelpreisverleihung 1929",
+		"original": "Jürgen Klinsmann zum Gewinn der Fußballeuropameisterschaft 1996"
+	},
+	"46": {
+		"quote": "Ich habe eigentlich keine besonderen Talente. Ich bin nur leidenschaftlich neugierig.",
+		"author": "Barack Obama",
+		"original": "Albert Einstein"
+	},
+	"47": {
+		"quote": "Der Mensch ist nichts anderes als das, wozu er sich macht.",
+		"author": "Optimus Prime",
+		"original": "Bertolt Brecht"
+	},
+	"48": {
+		"quote": "Wer kämpft, kann verlieren. Wer nicht kämpft, hat schon verloren.",
+		"author": "Karl May",
+		"original": "Jean-Paul Sartre"
+	},
+	"49": {
+		"quote": "We hold this truth to be self-evident, that all men are created evil.",
+		"author": "aus Lord Of The Things von J.R.R. Ewing",
+		"original": "Amerikanische Unabhängigkeitserklärung"
+	},
+	"50": {
+		"quote": "Der Mensch ist frei geboren und liegt doch überall in Ketten.",
+		"author": "Holiday Inn",
+		"original": "Jean-Jacques Rousseau"
+	},
+	"51": {
+		"quote": "Du riechst so gut.",
+		"author": "Jean-Baptiste Grenouille",
+		"original": "Lied von Rammstein"
+	},
+	"52": {
+		"quote": "Wir werden nicht schweigend in der Nacht untergehen. Wir werden nicht ohne zu kämpfen vergehen. Wir werden überleben. Wir werden weiterleben. Heute feiern wir gemeinsam unseren Independence Day!",
+		"author": "Ho Chi Minh",
+		"original": "Independence Day"
+	},
+	"53": {
+		"quote": "Dieses Kribbeln im Bauch, das man nie mehr vergisst, als ob da im Magen der Teufel los ist.",
+		"author": "Lieutenant Ellen Ripley",
+		"original": "Lied von Pe Werner"
+	},
+	"54": {
+		"quote": "Wer sich seiner Vergangenheit nicht erinnert, ist verurteilt, sie zu wiederholen.",
+		"author": "Rocky Balboa",
+		"original": "George Santayana"
+	},
+	"55": {
+		"quote": "I have a dream.",
+		"author": "Marc-Uwe Kling",
+		"original": "Martin Luther King"
+	},
+	"56": {
+		"quote": "Der Bericht über meinen Tod wurde stark übertrieben.",
+		"author": "Mark Zuckerberg",
+		"original": "Mark Twain"
+	},
+	"57": {
+		"quote": "Into exile, I must go. Failed, I have.",
+		"author": "Hillary Clinton",
+		"original": "Yoda"
+	},
+	"58": {
+		"quote": "Auch wenn man unterliegt, soll man es in Ehren tun.",
+		"author": "Donald Trump",
+		"original": "Niccolò Machiavelli"
+	},
+	"59": {
+		"quote": "Komm mit mir ins Abenteuerland. Der Eintritt kostet den Verstand.",
+		"author": "Donald Trump",
+		"original": "Song von Pur"
+	},
+	"60": {
+		"quote": "Stets weiß man für jedes menschliche Problem eine Lösung — sauber, einleuchtend, und falsch.",
+		"author": "David Cameron",
+		"original": "Henry Louis Mencken"
+	},
+	"61": {
+		"quote": "Dabei sein ist alles.",
+		"author": "Xavier Naidoo",
+		"original": "Grundprinzip von Olympia"
+	}
 };
